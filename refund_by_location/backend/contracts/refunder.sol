a7cb438acd4883e77d250db4584469f1a965fa23
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-07-15 17:16:16 -0700

    modify payer

--- refund_by_location/backend/contracts/refunder.sol
+++ refund_by_location/backend/contracts/refunder.sol
@@ -30,11 +30,11 @@ contract refunderContract{
       if(msg.sender == owner) selfdestruct(payable(owner));
     }
 
-    function check_existance(address employee) public constant returns(bool){
-      for(uint256 i = 0; i < employee.length; i++){
-         if(employees[i] == employee) return true;
-      }
-      return false;
+    function check_existance(address employee) private view returns(bool){
+        for(uint256 i = 0; i < employees.length; i++){
+            if(employees[i] == employee) return true;
+        }
+        return false;
     }
 
     function add_employee(address id, int256 lat, int256 lon, uint32 rad, uint8 fund, bool stat) public {
@@ -47,16 +47,20 @@ contract refunderContract{
         employees.push(id);
 
     }
+    function calculate_radius(int256 lat, int256 lon) private {
 
+    }
     function check_position(int256 lat, int256 lon) public {
         require(check_existance(msg.sender));
         uint32 new_radius = calculate_radius(lat, lon);
-        if(new_radius < radius){
-            contractInfo[img.address].status = true
+        if(new_radius < contractInfo[msg.sender].radius){
+            contractInfo[msg.address].status = true;
         }
     }
 
-    function pay(address payable add) public{
+    function pay(address payable add) private {
+        require(check_existance(msg.sender));
+        require(contractInfo[msg.address].status == true);
         add.transfer(contractInfo[add].budget);
         contractInfo[add].status=false;
     }
