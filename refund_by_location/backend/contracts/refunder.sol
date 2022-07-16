430be6fcc7b0bedda030a465fd07dbd686720302
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-07-16 03:50:27 -0700

    payable is not working

--- refund_by_location/backend/contracts/refunder.sol
+++ refund_by_location/backend/contracts/refunder.sol
@@ -16,7 +16,7 @@ contract refunderContract{
         //address emp_add;
         int256 center_lat;
         int256 center_lon;
-        uint32 radius;
+        int256 radius;
         uint8 budget;
         bool status;
      
@@ -24,7 +24,8 @@ contract refunderContract{
   
     mapping(address => contract_spec) public contractInfo;
 
-//Functions
+
+    //Functions
 
     function kill() public {
       if(msg.sender == owner) selfdestruct(payable(owner));
@@ -37,31 +38,48 @@ contract refunderContract{
         return false;
     }
 
-    function add_employee(address id, int256 lat, int256 lon, uint32 rad, uint8 fund, bool stat) public {
+    function sqrt(int256 input) private pure returns (int256 output) {
+        int256 interim = (input + 1) / 2;
+        output = input;
+        while (interim < output) {
+            output = interim;
+            interim = (input / output * 2) / 2;
+        }
+    }
+
+
+    function add_employee(address id, int256 lat, int256 lon, int256 rad, uint8 fund) public {
         require(!check_existance(id));
         contractInfo[id].center_lat = lat;
         contractInfo[id].center_lon = lon;
         contractInfo[id].budget = fund;
         contractInfo[id].radius = rad;
-        contractInfo[id].status = stat;
+        contractInfo[id].status = false;
         employees.push(id);
 
     }
-    function calculate_radius(int256 lat, int256 lon) private {
-
+    function calculate_radius(int256 lat, int256 lon, address adr) private view returns(int256) {
+        int256 radius = 0;
+        int256 x = lat - contractInfo[adr].center_lat;
+        int256 y = lon - contractInfo[adr].center_lon;
+        radius = sqrt(x**2 + y**2);
+        return radius;
     }
+
     function check_position(int256 lat, int256 lon) public {
-        require(check_existance(msg.sender));
-        uint32 new_radius = calculate_radius(lat, lon);
+        int256 new_radius = calculate_radius(lat, lon, msg.sender);
         if(new_radius < contractInfo[msg.sender].radius){
-            contractInfo[msg.address].status = true;
+            contractInfo[msg.sender].status = true;
+        }else{
+            contractInfo[msg.sender].status = false;
         }
     }
 
-    function pay(address payable add) private {
-        require(check_existance(msg.sender));
-        require(contractInfo[msg.address].status == true);
-        add.transfer(contractInfo[add].budget);
-        contractInfo[add].status=false;
+    function pay(address payable _to) public {
+        require(check_existance(_to));
+        require(contractInfo[_to].status == true);
+        bool sent = _to.send(contractInfo[_to].budget);
+        require(sent, "Failed to send Ether");
+        contractInfo[_to].status=false;
     }
 }
